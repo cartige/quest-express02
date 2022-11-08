0e28bb8299ba73f918ac8b590fdd091f8717989c
cartige <cartiaux.gerald@gmail.com>
2022-11-08 11:46:01 +0100

    express-quest 04bis

--- app.js
+++ app.js
@@ -1,31 +1,10 @@
 const express = require("express");
+const app = express();
 require("dotenv").config();
 
-const app = express();
 app.use(express.json());
 
 const port = process.env.APP_PORT ?? 5000;
-
-const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
-};
-
-app.get("/", welcome);
-
-const movieHandlers = require("./movieHandlers");
-const userHandlers = require("./userHandlers");
-
-app.get("/api/movies", movieHandlers.getMovies);
-app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", movieHandlers.postMovie);
-app.put("/api/movies/:id", movieHandlers.updateMovie);
-app.delete("/api/movies/:id", movieHandlers.deleteMovie);
-
-app.get("/api/users", userHandlers.getUsers);
-app.get("/api/users/:id", userHandlers.getUserById);
-app.post("/api/users", userHandlers.postUser);
-app.put("/api/users/:id", userHandlers.updateUser);
-app.delete("/api/users/:id", userHandlers.deleteUser);
 
 app.listen(port, (err) => {
   if (err) {
@@ -34,3 +13,24 @@ app.listen(port, (err) => {
     console.log(`Server is listening on ${port}`);
   }
 });
+
+const welcome = (req, res) => {
+  res.send("Welcome to my favourite movie list");
+};
+const movieHandlers = require("./movieHandlers");
+const userHandlers = require("./userHandlers");
+const { validateMovie, validateUser } = require("./validator");
+
+app.get("/", welcome);
+
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.post("/api/movies", validateMovie, movieHandlers.postMovie);
+app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
+app.post("/api/users", validateUser, userHandlers.postUser);
+app.put("/api/users/:id", validateUser, userHandlers.updateUser);
+app.delete("/api/users/:id", userHandlers.deleteUser);
--- userHandlers.js
+++ userHandlers.js
@@ -4,15 +4,15 @@ const getUsers = (req, res) => {
   let sql = "select * from users";
   const sqlValues = [];
 
-  if (req.query.language !== undefined) {
+  if (req.query.language != null) {
     sql += " where language = ?";
     sqlValues.push(req.query.language);
 
-    if (req.query.city !== undefined) {
+    if (req.query.city != null) {
       sql += " and city = ?";
       sqlValues.push(req.query.city);
     }
-  } else if (req.query.city !== undefined) {
+  } else if (req.query.city != null) {
     sql += " where city = ?";
     sqlValues.push(req.query.city);
   }
